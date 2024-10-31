701a8dce628d19cfdad6899bf2f197a6c1f0850a
hsamko <73651287+hsamko@users.noreply.github.com>
2024-10-31 19:59:26 +0300

    uoajfn;ooeyificb

---
+++ AppCenterAnalyticsSample/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppCenterAnalyticsSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("ios={Your iOS App secret here};" +
+                  "uwp={Your UWP App secret here};" +
+                  "android={Your Android App secret here}",
+                  typeof(Analytics));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
